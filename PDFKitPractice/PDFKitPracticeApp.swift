2174186747bc07d8972749ef47a02e2ca4a216b2
shinseungjae <mok05247@naver.com>
2025-01-20 14:23:23 +0900

    inital commit

---
+++ PDFKitPractice/PDFKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitPracticeApp.swift
+//  PDFKitPractice
+//
+//  Created by 신승재 on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PDFKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
